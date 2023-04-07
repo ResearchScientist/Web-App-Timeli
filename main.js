40dfb8f0af607072f0a7554ed9d8a70708c16c63
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2023-04-07 12:17:09 -0700

    content : add clock display logic

---
+++ main.js
@@ -0,0 +1,14 @@
+const timeDisplay = document.getElementById('time-announcement');
+
+function clock() {
+  let now = new Date();
+  let h = now.getHours();
+  let m = now.getMinutes();
+  let hh = h < 10 ? "0" + h : h;
+  let mm = m < 10 ? "0" + m : m;
+
+  timeDisplay.innerHTML = `${hh}:${mm}`;
+}
+
+setInterval(clock,1000);
+
