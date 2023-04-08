d73a34fd1fc1c94d8a68e783ab4092a5ed590209
ResearchScientist <8367366+ResearchScientist@users.noreply.github.com>
2023-04-07 17:28:05 -0700

    animation : switch moon and sun

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const timeDisplay = document.getElementById('time-announcement');
 const sunAndmoon = document.getElementById('sun-moon-lever');
+const sunMoon = document.getElementById('sun-moon');
 
 function clock() {
   let now = new Date();
@@ -14,6 +15,8 @@ function clock() {
   let rotationRate = (hratio * 360) - 90;
   timeDisplay.textContent = `${hh}:${mm}`;
   rotateSunMoon(rotationRate);
+  switchSunMoon(h,m);
+  // updateActions(h,m);
 }
 
 // function clock() {
@@ -25,3 +28,19 @@ function rotateSunMoon(rotationRate) {
 }
 
 setInterval(clock,1000);
+
+
+// * DATABASE LOGIC * //
+
+const actions = ['wake up','eat','read','do tech work','do research work','focus work','eat lunch!','keep eating','be creative','gym','gym','shower','eat'];
+
+// function updateActions(h,m) {
+//   console.log(`its ${h} ${m}`);
+//   console.log(h > 16)
+// }
+
+function switchSunMoon(h,m) {
+  if ( (h > 18) && (m > 29) ) {
+    sunMoon.style.setProperty('background-color','white');
+  }
+}
